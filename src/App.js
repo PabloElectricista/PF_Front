61d63a2a0c7ee87424d394687a69a54d18007bec
Pablo Spector <pablo_spector@hotmail.com>
2022-08-16 13:05:34 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Sound Good app</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/redux/actions/isloadingActions.js
@@ -0,0 +1,3 @@
+import { setIsLoading } from "../reducers/isLoadingSlice";
+
+export const setIsLoad = (isLoading) => setIsLoading(isLoading)
