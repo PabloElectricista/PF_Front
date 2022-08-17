15dc12b2ae9c96aefb748ff32ea6ad0b19a95ec0
JoaquinAngelino <joacoangelino@gmail.com>
2022-08-17 20:14:00 -0300

    Redux store fix

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,11 +1,11 @@
-//import axios from 'axios'
+import axios from 'axios'
 
-// export function getAllInstruments(){
-//     return async function(dispatch){
-//         const resu =  await axios('/instruments')
-//         dispatch({
-//             type: "GET_ALL_INSTRUMENTS",
-//             payload: resu.data
-//         })
-//     }
-// }
+export function getAllInstruments(){
+    return async function(dispatch){
+        const resu =  await axios('/instruments')
+        dispatch({
+            type: "GET_ALL_INSTRUMENTS",
+            payload: resu.data
+        })
+    }
+}
