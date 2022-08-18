c167bb01b65224f9d5ac73259e407af14a84708b
David-G18 <kdavidg1998@gmail.com>
2022-08-18 01:10:41 -0500

    the loading component was created

--- src/components/Loading/index.jsx
+++ src/components/Loading/index.jsx
@@ -1,9 +1,11 @@
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.css';
 
 export default function Loading() {
-
   return (
-    <>
-      <h1>Loading</h1>
-    </>
+    <div class="d-flex justify-content-center">
+      <div class="spinner-border text-primary m-5" role="status">
+      </div>
+    </div>
   )
 }
