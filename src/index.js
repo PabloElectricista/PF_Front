e94243e1a0bfce409078813457cffbd1fef0d46f
David-G18 <kdavidg1998@gmail.com>
2022-08-18 12:22:00 -0500

    Changes in front for deploy

--- src/index.js
+++ src/index.js
@@ -4,17 +4,21 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import store from './redux/store';
+import axios from 'axios';
+import dotenv from 'dotenv';
+dotenv.config();
+
+axios.defaults.baseURL = process.env.REACT_APP_API || "http://localhost:4000"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      
-        <App />
-     
+      <App />
     </Provider>
   </React.StrictMode>
 );
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
