e723cc9edcaf363058f2f64cf33d57d3b24a02ae
valdiviavv <valdiviavv@hotmail.com>
2022-08-20 16:27:29 -0400

    The favorites view was added. Also, the reducer's file was updated with the 'favoriteInstruments' array.

--- src/components/Favorites/index.jsx
+++ src/components/Favorites/index.jsx
@@ -1,9 +1,73 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import {useSelector} from "react-redux";
+import Pagination from "../Pagination";
+import ProductCard from "../Card";
+import './Favorites.css';
 
 
 function Favorites() {
+
+    const favoriteInstruments = useSelector(state => state.favoriteInstruments);
+    const pageSize = 15;
+    const totalPosts = favoriteInstruments.length;
+    const [pageNumber, setPageNumber] = useState(1);
+    const [currentPage, setCurrentPage] = useState([]);
+
+    useEffect( () =>{
+        if (favoriteInstruments.length !== 0 ) {
+            changePage(pageNumber);
+        }
+    }, [favoriteInstruments])
+
+    function changePage(newPageNumber) {
+        const indexEnd = newPageNumber * pageSize;
+        const indexIni = indexEnd - pageSize;
+        setCurrentPage(favoriteInstruments.slice(indexIni, indexEnd));
+        setPageNumber(newPageNumber);
+    }
+
+    function renderInstruments() {
+        if (favoriteInstruments.length === 0) {
+            return (
+                <h4>
+                    The favorites list is empty.
+                </h4>
+            );
+        }
+
+        let instrumentsMap = currentPage.map(instrument => <ProductCard
+            key={instrument.id}
+            id={instrument.id}
+            name={instrument.name}
+            price={instrument.price}
+            brand={instrument.brand}
+            rating={1}
+            image={instrument.image} />);
+
+        return (
+            <div>
+                <Pagination currentPage={currentPage}
+                            postPerPage={pageSize}
+                            totalPosts={totalPosts}
+                            paginate={changePage}/>
+
+                <div className="favoriteCards">
+                    {instrumentsMap}
+                </div>
+                <Pagination currentPage={currentPage}
+                            postPerPage={pageSize}
+                            totalPosts={totalPosts}
+                            paginate={changePage}/>
+            </div>
+        );
+    }
+
+
     return (
-        <div>Favorites</div>
+        <div className="containerHome">
+            <h1>Favorites</h1>
+            {renderInstruments()}
+        </div>
     );
 }
 
