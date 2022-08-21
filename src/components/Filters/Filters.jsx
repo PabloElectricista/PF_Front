29b2f7b56b8537858b56b7a98c0f20a95f3e0ecc
David-G18 <kdavidg1998@gmail.com>
2022-08-20 21:30:33 -0500

    Change style in home with his components

--- src/components/Filters/Filters.jsx
+++ src/components/Filters/Filters.jsx
@@ -5,19 +5,19 @@ import './Filters.css';
 export default function Filters (props) {
     return(
         <Accordion className="accordion" defaultActiveKey={['0']} alwaysOpen>
-            <Accordion.Item eventKey="0">
+            <Accordion.Item eventKey="1">
                 <Accordion.Header>Filtro #1</Accordion.Header>
                 <Accordion.Body>
                 Esta es la prueba para el filtro 1
                 </Accordion.Body>
             </Accordion.Item>
-            <Accordion.Item eventKey="1">
+            <Accordion.Item eventKey="2">
                 <Accordion.Header>Filtro #2</Accordion.Header>
                 <Accordion.Body>
                 Esta es la prueba para el filtro 2
                 </Accordion.Body>
             </Accordion.Item>
-            <Accordion.Item eventKey="2">
+            <Accordion.Item eventKey="3">
                 <Accordion.Header>Filtro #3</Accordion.Header>
                 <Accordion.Body>
                 Esta es la prueba para el filtro 3
