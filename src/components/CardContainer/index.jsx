97758d61202500c24285faa1f18ffbaae4fa4ae7
Martín Araujo <98348984+MNAHEAVY@users.noreply.github.com>
2022-08-25 20:28:49 -0300

    [mnaOrder/logica ordenamiento]

--- src/components/CardContainer/index.jsx
+++ src/components/CardContainer/index.jsx
@@ -1,11 +1,13 @@
 import { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
+import {orderComponentsByPrice, orderComponentsByName } from '../../redux/actions'
+import Accordion from 'react-bootstrap/Accordion';
 import './CardContainer.css';
 import Pagination from "../Pagination";
 import Loading from "../Loading";
 import Filters from "../../components/Filters/Filters";
 import ProductCard from '../Card/index';
-import Dropdown from 'react-bootstrap/Dropdown';
+
 
 
 
@@ -13,13 +15,10 @@ export default function CardContainer() {
   const dispatch = useDispatch()
   const allInstruments = useSelector(state => state.instruments)
   const [currentPage, setCurrentPage] = useState(1);
-
   const [refresh, setRefresh] = useState(1)
 
   useEffect(() => {
-    
-    setCurrentPage(1)
-    setRefresh(refresh + 1)
+  setCurrentPage(1)
   }, [allInstruments])
 
   useEffect(() => {
@@ -39,21 +38,49 @@ export default function CardContainer() {
     rating={1}
     image={instrument.image} />)
 
-  const paginate = (number) => { setCurrentPage(number) }
+  const paginate = (number) => { setCurrentPage(number) };
+
+
+
+function handleOrderPrice(e){
+    e.preventDefault();
+    setRefresh(refresh+1)
+    dispatch(orderComponentsByPrice(e.target.value))
+}
+function handleOrderName(e){
+  e.preventDefault();
+  setRefresh(refresh+1)
+    dispatch(orderComponentsByName(e.target.value))
+}
 
   return (
-    <div className="containerHome">
-      <Dropdown className="orderButton" size="sm">
-        <Dropdown.Toggle variant="success" className="toglleDropDown" id="dropdown-basic">
-          Order By
-        </Dropdown.Toggle>
 
-        <Dropdown.Menu>
-          <Dropdown.Item >Ordenamiento 1</Dropdown.Item>
-          <Dropdown.Item >Ordenamiento 2</Dropdown.Item>
-          <Dropdown.Item >ORdenamiento 3</Dropdown.Item>
-        </Dropdown.Menu>
-      </Dropdown>
+    
+    <div >
+        <Accordion>
+    <Accordion.Item eventKey="3">
+    <Accordion.Header>Order By</Accordion.Header>
+    <Accordion.Body>
+    <div>
+             <select onChange={(e) => {handleOrderPrice(e)}}>
+                 <option disabled>"Price"</option>
+                 <option value="All" >"All"</option>
+                 <option value="Higher price">"Higher price"</option>
+                 <option value="Lower price">"Lower price"</option>
+             </select>
+         </div>
+         <div>
+             <select onChange ={(e) => {handleOrderName(e)}}>
+                 <option disabled>"Name"</option>
+                 <option value = "All">"All"</option>
+                 <option value="Up to Down">"Up to Down"</option>
+                 <option value="Down to Up">"Down to Up"</option>
+             </select>
+         </div>
+    </Accordion.Body>
+</Accordion.Item>
+</Accordion>
+             
 
       {mapInstruments ?
         <div className="containerContent">
