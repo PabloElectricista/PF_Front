f0581d65ae920fc922f076fb332e8b570006c8e0
JoaquinAngelino <joacoangelino@gmail.com>
2022-09-07 18:55:48 -0300

    guardado

--- src/components/UserProducts/UserProducts.jsx
+++ src/components/UserProducts/UserProducts.jsx
@@ -1,56 +1,58 @@
 import { useAuth0 } from "@auth0/auth0-react"
 import { useEffect } from "react"
 import { useDispatch, useSelector } from "react-redux"
-import { getAllProducts } from "../../redux/actions"
+import { getAllProducts, getUserByEmail } from "../../redux/actions"
 import NothingFound from "../NothingFound/NothingFound"
 import UserProductCard from "./UserProductCard"
 
 export default function UserProducts() {
   const { user, isAuthenticated } = useAuth0()
   const myUser = useSelector(state => state.usersEmail)
-  const allProducts = useSelector(state => state.allProducts)
+  const allProducts = useSelector(state => state.allInstruments)
   console.log(myUser)
-  const MyProducs = myUser.products
   const dispatch = useDispatch()
-
+  let myProducts = []
+  useEffect(() => {
+    if (isAuthenticated) {
+      dispatch(getUserByEmail(user.email))
+    }
+    dispatch(getAllProducts())
+  }, [dispatch, isAuthenticated])
   useEffect(() => {
     dispatch(getAllProducts())
   }, [dispatch])
 
 
-  // if (isAuthenticated) {
 
-  //   console.log(products.map(e => e.user));
-  //   const myProducts = products.filter(product => product.user._id === user.sub.slice(user.sub.indexOf("|") + 1))
-  //   if (myProducts.length === 0) {
-  //     return (
-  //       <>
-  //       <h2>My products</h2>
-  //       <NothingFound />
-  //       </>
-  //     )
-  //   }
-  //   let mapProducts = myProducts.map(instrument => {
-  //     return (
-  //       <UserProductCard
-  //         key={instrument._id}
-  //         id={instrument._id}
-  //         name={instrument.name}
-  //         price={instrument.price}
-  //         brand={instrument.brand}
-  //         rating={Math.floor((Math.random() * 6))}
-  //         image={instrument.image}
-  //       />
-  //     )
-  //   })
-  //   return (
-  //     <>
-  //       {mapProducts}
-  //     </>
-  //   )
-  // }
-
+  if (myUser.products && allProducts.length) {
+    myProducts = myUser.products.map(e => allProducts.find(product => product._id === e))
+    myProducts = myProducts.filter(e => e !== undefined)
+  }
+  if (!myProducts || myProducts.length === 0){
+    return (
+      <>
+        <h2>My products</h2>
+        <NothingFound />
+      </>
+    )
+  }
+  console.log(myProducts)
+  let mapProducts = myProducts.map(instrument => {
+    return (
+      <UserProductCard
+        key={instrument._id}
+        id={instrument._id}
+        name={instrument.name}
+        price={instrument.price}
+        brand={instrument.brand}
+        rating={Math.floor((Math.random() * 6))}
+        image={instrument.image}
+      />
+    )
+  })
   return (
-    <h2>You have no products published</h2>
+    <>
+      {mapProducts}
+    </>
   )
 }
