4fac966ebeb17e3ff6610bb4ad491039ff9198b2
JoaquinAngelino <joacoangelino@gmail.com>
2022-09-07 01:47:44 -0300

    update paypall para david otra vez

--- src/components/paypal/Paypal.jsx
+++ src/components/paypal/Paypal.jsx
@@ -4,37 +4,36 @@ import { getPrice } from '../Card/favAndCart'
 export default function Paypal() {
   const paypal = useRef();
 
+
   useEffect(() => {
-    window.paypal
-      .Buttons({
-        createOrder: (data, actions, err) => {
-          return actions.order.create({
-            intent: "CAPTURE",
-            purchase_units: [
-              {
-                description: "Instrument, PRUEBA",
-                amount: {
-                  currency_code: "USD",
-                  value: getPrice(),
-                },
+    window.paypal.Buttons({
+      createOrder: (data, actions, err) => {
+        return actions.order.create({
+          intent: "CAPTURE",
+          purchase_units: [
+            {
+              description: "Instrument, PRUEBA",
+              amount: {
+                currency_code: "USD",
+                value: getPrice(),
               },
-            ],
-          });
-        },
-        onApprove: async (data, actions) => {
-          const order = await actions.order.capture();
-          console.log(order);
-        },
-        onError: (err) => {
-          console.log(err);
-        },
-      })
-      .render(paypal.current);
-  }, []);
+            },
+          ],
+        });
+      },
+      onApprove: async (data, actions) => {
+        const order = await actions.order.capture();
+        console.log(order);
+      },
+      onError: (err) => {
+        console.log(err);
+      },
+    }).render(paypal.current)
+  },[]);
 
   return (
-    
-     <div ref={paypal}></div>
-    
+
+    <div ref={paypal}></div>
+
   );
 }
