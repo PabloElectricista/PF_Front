f040cc1204d413d8a8da0adfa578e0a9a389093f
Martín Araujo <98348984+MNAHEAVY@users.noreply.github.com>
2022-09-08 00:43:42 -0300

    Update auth.js

--- src/auth.js
+++ src/auth.js
@@ -1,5 +1,2 @@
 export const authCredentials = {
-    domain: 'dev-mcpsqmn.us.auth0.com',
-    clientId: 'rHF1dDegOfQJCNzyveZInhjSm9bzerOq',
-    audience:"https://dev--mcpsqmn.us.auth0.com/api/v2/"
-}
+    domain:'dev--mcpsqmn.us.auth0.com', clientId:'rHF1dDegOfQJCNzyveZlnhjSm9bzerOq' }
